b87f74aa05fbc4fe36d3f5a8a0669875214e32a4
pruslanov <petr.ruslanov@gmail.com>
2023-07-30 01:16:19 +0300

    MAIN branche, test.sh modifided the same SECOND RAW.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # This id first row of file under DEV branche. Creation of the file
-# DEV branch add one more second raw
+# MAIN branch add one moreconflict in second raw
 # DEV branch add third row to the file
