fc8cd32986dd7f9ad66a9c7d69af71dc4ac30b3b
pruslanov <petr.ruslanov@gmail.com>
2023-07-30 01:02:10 +0300

    CONFLICT branche, test.sh modifided.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # This id first row of file under DEV branche. Creation of the file
-# DEV branch add one more second raw
+# CONFICT branch UPDATING EXISTING SECOND ROW
 # DEV branch add third row to the file
